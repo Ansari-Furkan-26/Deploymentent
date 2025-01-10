b4c2889d64e4b48bbf02e6456375f12ae68f8086
Furqan Ansari <ansarifurkanabcd786@gmail.com>
2025-01-10 16:48:34 +0530

    Backend Delovment

--- Taskify-backend/config/db.js
+++ Taskify-backend/config/db.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+// Get MongoDB URI from environment variables
+const mongoURI = process.env.MONGO_URI;
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Adjust the timeout for the connection
+      socketTimeoutMS: 45000, // Adjust socket timeout (in ms)
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
--- Taskify-backend/server.js
+++ Taskify-backend/server.js
@@ -79,5 +79,7 @@ app.delete('/api/tasks/:id', async (req, res) => {
 // 
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
